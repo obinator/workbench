c89647d285fad497fc9f26b4ac9002de72195232
Scott Bellware <sbellware@gmail.com>
2012-08-21 22:37:31 -0500

    - Class archetypes aren't part of the class name (suffix) - Suffixes have been replaced with namespaces

---
+++ lib/workbench/data.rb
@@ -0,0 +1,1 @@
+require 'workbench/data/shipping'
---
+++ lib/workbench/data/shipping.rb
@@ -0,0 +1,10 @@
+module Workbench
+  module Data
+    class Shipping
+      attr_accessor :order_id
+      attr_accessor :customer_id
+      attr_accessor :product_ids_in_order
+      attr_accessor :customer_billed
+    end
+  end
+end
